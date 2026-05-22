9880a0b2b3c39c4a72842174d5b76db36f68722b
rakhaallea <rakhaallea@gmail.com>
2026-05-22 09:27:29 +0700

    feat: add login page dekstop layout, mobile and show password

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const menu_hamburger = document.querySelector(".ham-icon");
-const nav_list = document.querySelector(".nav-list");
+const menuHamburger = document.querySelector(".ham-icon");
+const navList = document.querySelector(".nav-list");
 
-menu_hamburger.addEventListener("click", () => {
-    nav_list.classList.toggle("show-nav");
-    console.log(nav_list)
+if (menuHamburger && navList) {
+    menuHamburger.addEventListener("click", () => {
+        navList.classList.toggle("show-nav");
+    });
+}
+
+const passwordIcon = document.getElementById("show-icon");
+const passwordInput = document.getElementById("password");
+
+if (passwordIcon && passwordInput) {
+    passwordIcon.addEventListener("click", () => {
+        if (passwordInput.type === "password") {
+            passwordIcon.innerHTML = "visibility_off"
+            passwordInput.type = 'text'
+        } else {
+            passwordIcon.innerHTML = "visibility"
+            passwordInput.type = 'password'
+        }
 })
-
+}
 
 window.addEventListener("scroll", () => {
     const navbar = document.querySelector('nav');
